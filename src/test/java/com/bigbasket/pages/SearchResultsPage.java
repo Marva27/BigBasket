33b577e031592e5a062301c59a8e2bbbf8846931
srinivasanramu <srinivasanramu@192.168.0.104>
2016-07-02 11:23:58 -0400

    Running only Products suite

--- src/test/java/com/bigbasket/pages/SearchResultsPage.java
+++ src/test/java/com/bigbasket/pages/SearchResultsPage.java
@@ -288,8 +288,8 @@ public class SearchResultsPage {
 				}
 			}
 			if(!filterWorks){
-				UtilFunctions.writeLog(log, Constants.FAIL+" - "+productName+" is out of range. "+"Original product price is: "+productPrice+" Offer product price is: "+convertedOfferProductPrice+" % discounted is: "+percentageDiscounted);
-				return Constants.FAIL+" - "+productName+" is out of range. "+"Original product price is: "+productPrice+" Offer product price is: "+convertedOfferProductPrice+" % discounted is: "+percentageDiscounted;
+				UtilFunctions.writeLog(log, Constants.FAIL+" - "+productName+" is out of range. "+"\n Original product price is: "+convertedOriginalProductPrice+"\n Offer product price is: "+convertedOfferProductPrice+" % discounted is: "+percentageDiscounted);
+				return Constants.FAIL+" - "+productName+" is out of range. "+"\n Original product price is: "+convertedOriginalProductPrice+"\n Offer product price is: "+convertedOfferProductPrice+" % discounted is: "+percentageDiscounted;
 			}else{
 				UtilFunctions.writeLog(log, Constants.PASS);
 				return Constants.PASS;
